11f612a62607c04e4814251509f1ea9636daa295
otash16 <otajonqutbiddinov1@gmail.com>
2024-05-14 15:29:06 +0500

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,28 @@
+console.log("Web Serverni Boshlash")
+const express = require('express');
+const app = express();
+
+//1 : Kirish Code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extend: true}));
+
+//2: Session
+
+//3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4: Routing code
+app.get("/", function(req, res){
+    res.end(`<h1>Hello World!</h1>`);
+});
+app.get("/gift", function(req, res){
+    res.end(`<h1>Siz sovg'alar bo'limidasiz`);
+})
+
+const server = http.createServer(app);
+let Port = 3000;
+server.listen(Port, function(){
+    console.log(`The server is succesfully running on port ${Port}`);
+});
