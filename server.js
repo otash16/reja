fda0fee023cbca5df429ac32b2118b49875c70a5
otash16 <otajonqutbiddinov1@gmail.com>
2024-05-27 18:20:05 +0500

    Delete and Crud operations

--- server.js
+++ server.js
@@ -1,57 +1,28 @@
-console.log("Web Serverni Boshlash")
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
-const http = require('http');
-const fs = require('fs');
-let user;
-fs.readFile("database/user.json", 'utf8', (err, data) => {
-    if(err){
-        console.log(err);
-    }else{
-        user = JSON.parse(data);
+const http = require("http");
+const mongodb = require("mongodb");
+let db;
+const connectionString =
+  "mongodb+srv://otajonqutbiddinov1:PES2021pes2021@cluster0.mj2gqmh.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      //console.log(client);
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(3000, function () {
+        console.log(
+          `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
     }
-});
-
-//1 : Kirish Code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extend: true}));
-
-//2: Session
-
-//3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4: Routing codeapp.get("/", function(){
-    app.post("/create-item", (req, res) => {
-        console.log("user entered / create-item");
-        const new_reja = req.body.reja;
-        db.collection("plans").insertOne({reja : new_reja}, (err, data) => {
-            res.json(data.ops[0]);
-        })
-
-        app.post("/delete-item", (req, res) => {
-            const id = req.body.id;
-            db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data){
-                res.json({state: "success"})
-            })
-
-        });
-    console.log(req.body);
-    res.json({test : "succes"});
-    })
-app.get('/', function(req, res){
-    res.render('harid');
-});
-
-app.get('/author', (req, res) => {
-    res.render('author', {user: user});
-})
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`The server is succesfully running on port ${PORT}`);
-});
+  }
+);
--- train.js
+++ train.js
@@ -1,150 +1,208 @@
+// MITASK-C
 
-// const list1 = [
-//     "Yaxshi talaba bo'ling", //0-20
-//     "To'g'ri boshliq tanlang va ko'proq xato qiling", //20-30
-//     "endi dam oling, endi foydasi yoq" //60+
-// ];
-// function maslahatBering(a, callback){
-//     if(typeof a != 'number') callback("Xatolik!!!", null);
-//     else if(a<=20) callback(null, list1[0]);
-//     else if(a>20 && a<=30) callback(null, list1[1]);
-//        else{
-//         setTimeout(function(){
-//             callback(null, list1[2])
-//         },2000);
-//        }
-// }
-// maslahatBering(88,(err, data) => {
-//     if(err) console.log("XATOLIK!!!", err);
-//     console.log("JAVOB:", data)
-// })
-
-
-
-
-
-
-
-
-
-// // A-TASK: 
-
-// // Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
-// // MASALAN countLetter("e", "engineer") 3ni return qiladi.
-// const list = ['engineer'];
-
-// function countDigits(str1, callback){
-//     let count = 0;
-//     for(let i=0; i<=str1.length; i++){
-//         if(str1[i] >= 0){
-//             count++;
-//         }
-//     }
-//     callback(null, count)
-// }
-// countDigits('ad2a54y79wet0sfgb9', (err, data) => {
-//     console.log(data)
-// });
-
-// const list1 = [
-//     "Yaxshi talaba bo'ling", //0-20
-//     "To'g'ri boshliq tanlang va ko'proq xato qiling", //20-30
-//     "endi dam oling, endi foydasi yoq" //60+
-// ];
-//  async function maslahatBering(a){
-//     if(typeof a != 'number') throw new Error("Xatolik!!!");
-//     else if(a<=20) return(null, list1[0]);
-//     else if(a>20 && a<=30) return(list1[1]);
-//        else{
-//         return  new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 resolve(list1[2]);
-//             },5000)
-//         })
-//     }
-//        }
-
-// then//catch
-// maslahatBering(25)
-// .then((data) => {
-//     maslahatBering(30)
-// .then((data) => {
-//     maslahatBering(67)
-// .then((data) => {
-//     console.log("Javob = ",data);
-// })
-// .catch((err) => {
-//     console.log("Xatolik", err);
-// })
-//     console.log("Javob = ",data);
-// })
-// .catch((err) => {
-//     console.log("Xatolik", err);
-// })
-//     console.log("Javob = ",data);
-// })
-// .catch((err) => {
-//     console.log("Xatolik", err);
-// })
-
-// async function run() {
-//     let javob = await maslahatBering(20);
-//     console.log(javob);
-//     javob = await maslahatBering(31);
-//     console.log(javob);
-//     javob = await maslahatBering(41);
-//     console.log(javob);
-// }
-// run();
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq,
+//  biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va
+// 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 
 class Shop {
     constructor(non, lagmon, cola) {
-        this.products = {
-            non: non,
-            lagmon: lagmon,
-            cola: cola
-        };
+      this.products = {
+        non: non,
+        lagmon: lagmon,
+        cola: cola,
+      };
     }
-
-    moment() {
-        const now = new Date();
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        return {
-            format: () => `${hours}:${minutes}`
-        };
+  
+    getCurrentTime() {
+      let now = new Date();
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      return `${hours}:${minutes}`;
     }
-
+  
     qoldiq() {
-        const time = this.moment().format();
-        const { non, lagmon, cola } = this.products;
-        return `Hozir ${time}da ${non}ta non, ${lagmon}ta lagmon va ${cola}ta cola mavjud!`;
+      const { non, lagmon, cola } = this.products;
+      return `Hozir ${this.getCurrentTime()}da ${non} ta non, ${lagmon} ta lagmon va ${cola} ta cola bor.`;
     }
-
-    sotish(product, amount) {
-        const time = this.moment().format();
-        if (this.products[product] !== undefined && this.products[product] >= amount) {
-            this.products[product] -= amount;
-            console.log(`Hozir ${time}da ${amount}ta ${product} sotildi!`);
-        } else {
-            console.log(`Hozir ${time}da ${product} yetarli emas yoki mavjud emas!`);
-        }
+  
+    sell(product, quntity) {
+      let keysArray = Object.keys(this.products);
+      if (!keysArray.includes(product)) {
+        return `Bizda hozir ${product} yo'q`;
+      }
+      if (this.products[product] < quntity) {
+        return `Bizda yetarli miqdorda ${product} yo'q`;
+      }
+      this.products[product] -= quntity;
+      return `Biz ${this.getCurrentTime()}da ${quntity} ta ${product} sotdik`;
     }
-
-    qabul(product, amount) {
-        const time = this.moment().format();
-        if (this.products[product] !== undefined) {
-            this.products[product] += amount;
-            console.log(`Hozir ${time}da ${amount}ta ${product} qabul qilindi!`);
-        } else {
-            console.log(`Hozir ${time}da ${product} mavjud emas!`);
-        }
+  
+    apply(product, quantity) {
+      let keysArray = Object.keys(this.products);
+      if (!keysArray.includes(product)) {
+        return `Bizda hozir ${product} yo'q`;
+      }
+      this.products[product] += quantity;
+      return `Biz ${this.getCurrentTime()}da ${quantity} ta ${product} qabul qildik`;
     }
-}
-
-// Misol uchun ishlatish:
-const shop = new Shop(4, 5, 2);
-console.log(shop.qoldiq()); // hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud!
-shop.sotish('non', 3); // hozir 20:40da 3ta non sotildi!
-shop.qabul('cola', 4); // hozir 20:40da 4ta cola qabul qilindi!
-console.log(shop.qoldiq()); // hozir 20:40da 1ta non, 5ta lagmon va 6ta cola mavjud!
+  }
+  
+  const shop = new Shop(4, 5, 2);
+  console.log(shop.qoldiq());
+  console.log(shop.sell("cola", 3));
+  console.log(shop.qoldiq());
+  console.log(shop.apply("non", 3));
+  console.log(shop.qoldiq());
+  console.log(shop.apply("lagmon", 3));
+  console.log(shop.sell("pepsi", 3));
+  console.log(shop.qoldiq());
+  
+  // Node js => single thread -> thread pool 4ta, - bir xonali , kam xarajatli, togri ishlatish kk - aynch, call back
+  // php multi thread
+  
+  // console.log("Jack Ma maslahatlari");
+  // const list = [
+  //   "yaxshi talaba boling", // 0-20
+  //   "togri boshliq tanlang va koproq hato qiling", //20-30
+  //   "uzingizga ishlashni boshlang", //30-40
+  //   "siz kuchli bolgan narsalarni qiling", //40-50
+  //   "yoshlarga investitsiya qiling", //50-60
+  //   "endi dam oling", //60
+  // ];
+  
+  // function maslahatBering(a, callback) {
+  //   if (typeof a !== "number") callback("Insert number", null);
+  //   else if (a <= 20) callback(null, list[0]);
+  //   else if (a > 20 && a <= 30) callback(null, list[1]);
+  //   else if (a > 30 && a <= 40) callback(null, list[2]);
+  //   else if (a > 40 && a <= 50) callback(null, list[3]);
+  //   else if (a > 50 && a <= 60) callback(null, list[4]);
+  //   else {
+  //     setInterval(function () {
+  //       callback(null, list[5]);
+  //     }, 1000);
+  //   }
+  // }
+  
+  // console.log("passed here 0");         // parametr sifatida function ishga tushadi
+  // maslahatBering(65, (err, data) => {
+  //   if (err) console.log("ERROR:", err);
+  //   else {
+  //     console.log("javob:", data);
+  //   }
+  // });
+  // console.log("passed here 1");
+  
+  // Callback functions
+  // function maslahatBering(a, callback) {
+  //   if (typeof a !== "number") callback("Insert number", null);
+  //   else if (a <= 20) callback(null, list[0]);
+  //   else if (a > 20 && a <= 30) callback(null, list[1]);
+  //   else if (a > 30 && a <= 40) callback(null, list[2]);
+  //   else if (a > 40 && a <= 50) callback(null, list[3]);
+  //   else if (a > 50 && a <= 60) callback(null, list[4]);
+  //   else {
+  //     setTimeout(function () {
+  //       callback(null, list[5]);
+  //     }, 5000);
+  //   }
+  // }
+  
+  // console.log("passed here 0");
+  // maslahatBering(65, (err, data) => {
+  //   if (err) console.log("ERROR:", err);
+  //   else {
+  //     console.log("javob:", data);
+  //   }
+  // });
+  // console.log("passed here 1");
+  
+  //Asynchrous functions    - sync ishga tushgandan keyin ishga tushadi, single thread ni band qilmaydi
+  // async function maslahatBering(a) {
+  //   if (typeof a !== "number") throw new Error("Insert  a number");
+  //   else if (a <= 20) return list[0];
+  //   else if (a > 20 && a <= 30) return list[1];
+  //   else if (a > 30 && a <= 40) return list[2];
+  //   else if (a > 40 && a <= 50) return list[3];
+  //   else if (a > 50 && a <= 60) return list[4];
+  //   else {
+  //     // return list[5];
+  //     return new Promise((resolve, reject) => {
+  //       //   setTimeout(() => {
+  //       setInterval(() => {
+  //         resolve(list[5]);
+  //       }, 1000);
+  //     });
+  //     // return(list[5]);
+  // setTimeout(function () {                               // promise functions da setTimeout, setInterval ishlaydi
+  //   return list[5];
+  // }, 5000);
+  //   }
+  // }
+  
+  // call viathen / catch
+  // console.log("passed here 0");
+  // maslahatBering(20)
+  //   .then((data) => {
+  //     console.log("javob:", data);
+  //   })
+  //   .catch((err) => {
+  //     console.log("ERROR:", err);
+  //   });
+  // console.log("passed here 1");
+  
+  // async function synchronus functionlar to'liq ishga tushib bolgach, async function
+  // natijalari bn Node Js ishlay boshlaydi, shu sabab single thread ni band qilmaydi event loop orqali thrad poolga tashlayveradi
+  
+  // call via asyn / await
+  // async function run() {
+  //   let javob = await maslahatBering(65); // ketma ket amalga oshiriladi, function  amalga oshmaguncha keyingisiga otmaydi
+  //   console.log(javob);
+  //   javob = await maslahatBering(31);
+  //   console.log(javob);
+  //   javob = await maslahatBering(41);
+  //   console.log(javob);
+  // }
+  // run();
+  
+  // Exercise
+  
+  // function countElement(letter, value) {
+  //   const text = letter.toLowerCase();
+  //   let n = 0;
+  //   for (let i = 0; i < value.length; i++) {
+  //     if (value[i].toLowerCase() === text) {
+  //       n++;
+  //     }
+  //   }
+  //   return n;
+  // }
+  // console.log(countElement("E", "element"));
+  
+  /* B-TASK: 
+  Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+  MASALAN countDigits("acs44ewe67t0s6fgb9") 7ni return qiladi.
+  */
+  
+  /*
+  function countDigits(a) {
+    let count = 0;
+  
+    for (let i = 0; i < a.length; i++) {
+      if (!isNaN(parseInt(a[i]))) {
+        count++;
+      }
+    }
+  
+    if (count === 0) {
+      return `There is no number in string`;
+    } else {
+      return count;
+    }
+  }
+  
+  console.log(countDigits("acs44ewe67t0s6fgb9"));
+  console.log(countDigits("www333ooo"));
+  console.log(countDigits("I study English 2022"));
+  */
